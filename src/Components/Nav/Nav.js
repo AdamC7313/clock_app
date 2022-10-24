34a7eab66ec28040f104bf0721b57d7db0af38a2
AdamC7313 <AdamC7313@gmail.com>
2022-10-24 16:24:41 -0400

    Nav changes

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -2,29 +2,20 @@ import './Nav.css';
 import { NavLink } from 'react-router-dom';
 
 export default function Nav() {
-    const style = { color: 'red', fontSize: '2em'}
     return (
         <div className='navbar'>
-        
             <NavLink to='/stopwatch'>
-            <div className="nav firstnav"><p>Stopwatch</p></div>
+            <div className="nav firstnav"><p><span>Stopwatch</span></p></div>
             </NavLink>
-        
-            
             <NavLink to='/timer'>
             <div className="nav middlenav">Timer</div>
             </NavLink>
-           
-            
             <NavLink to='/clock'>
             <div className="nav middlenav">Clock</div>
             </NavLink>
-            
-            
             <NavLink to='/alarm'>
             <div className="nav lastnav">Alarm</div>
              </NavLink>
-             
         </div>
     )
 }
