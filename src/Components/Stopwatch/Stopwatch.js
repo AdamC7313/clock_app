a89bfa853a7fda9d95e9698f89630ac1a59bd243
AdamC7313 <AdamC7313@gmail.com>
2022-10-24 12:57:07 -0400

    boiler plate and add nav/stopwatch components

---
+++ src/Components/Stopwatch/Stopwatch.js
@@ -0,0 +1,14 @@
+import './Stopwatch.css';
+
+export default function Stopwatch() {
+    return (
+        <div>
+            <div>
+                clock
+            </div>
+            <button>Start</button>
+            <button>Stop</button><br />
+            <button>Lap</button>
+        </div>
+    )
+}
