d00b31792cce96ea94103d43c8d16252512519e8
AdamC7313 <AdamC7313@gmail.com>
2022-10-26 15:15:37 -0400

    complete clock component

--- src/Components/Clock/Clock.js
+++ src/Components/Clock/Clock.js
@@ -1,14 +1,55 @@
 import './Clock.css';
+import { useEffect, useState } from 'react';
 
 export default function Clock() {
+    let [hour  , setHour  ] = useState(0);
+    const [minute, setMinute] = useState(0);
+    const [second, setSecond] = useState(0);
+    const [day   , setDay   ] = useState(0);
+    const [pm    , setPm    ] = useState(false);
+    const [timezone, setTimezone] = useState('ET')
+
+    useEffect(() => {
+
+    const update = () => {
+        const date = new Date();
+        let hour = date.getHours();
+        if(hour > 12) {
+            hour = (hour % 12) || 12;
+        }
+        setHour(hour);
+        setMinute(date.getMinutes());
+        setSecond(date.getSeconds());
+        setDay(date.getDay());
+        setPm(date.getHours() >= 12);
+    }
+
+    update();
+
+    const interval = setInterval(() => {
+        update();
+    }, 1000);
+
+        return ()=>clearInterval(interval);
+
+    }, []);
+
+
     return (
         <div>
-            <div>
-                clock
+            <div className='time-and-timezone'>
+            <div className='current-time'>
+                {`${hour}:${('0' + minute).slice(-2)}:${('0' + second).slice(-2)}`}
+                <span className='AMPM'>{hour > 12 ? 'AM' : 'PM'}</span>
+            </div><br />
+            {/* <h2 className='timezone-words'>Choose Timezone:</h2>
+            <select name='timezone' id='choose-timezone'>
+                <option value='ET'>ET</option>
+                <option value='CT'>CT</option>
+                <option value='MT'>MT</option>
+                <option value='PT'>PT</option>
+            </select> */}
             </div>
-            <button>Start</button>
-            <button>Stop</button><br />
-            <button>Lap</button>
         </div>
     )
 }
